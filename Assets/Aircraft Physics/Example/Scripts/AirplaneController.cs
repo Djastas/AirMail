df4ea90e4cf11e9f0aac00eecd2b1330f00c880d
Djastas <timebrocop@gmail.com>
2023-04-24 15:15:56 +0500

    it work

--- Assets/Aircraft Physics/Example/Scripts/AirplaneController.cs
+++ Assets/Aircraft Physics/Example/Scripts/AirplaneController.cs
@@ -40,54 +40,41 @@ public class AirplaneController : MonoBehaviour
 
     private void Update()
     {
-        pitch = Input.GetAxis("Vertical");
-        roll = Input.GetAxis("Horizontal");
-        yaw = Input.GetAxis("Yaw");
-    
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _thrustPercent = _thrustPercent > 0 ? 0 : 1f;
-        }
-    
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            flap = flap > 0 ? 0 : 0.3f;
-        }
-    
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            _brakesTorque = _brakesTorque > 0 ? 0 : 100f;
-        }
-    
-        displayText.text = "V: " + ((int)_rb.velocity.magnitude).ToString("D3") + " m/s\n";
-        displayText.text += "A: " + ((int)transform.position.y).ToString("D4") + " m\n";
-        displayText.text += "T: " + (int)(_thrustPercent * 100) + "%\n";
-        displayText.text += _brakesTorque > 0 ? "B: ON" : "B: OFF";
-    }
-
-    public void UpdateInput(float pitch,float roll,float yaw)
-    {
-        // this.pitch = pitch;
-        // this.roll = roll;
-        // this.yaw = yaw;
-
+        // pitch = Input.GetAxis("Vertical");
+        // roll = Input.GetAxis("Horizontal");
+        // yaw = Input.GetAxis("Yaw");
+        //
         // if (Input.GetKeyDown(KeyCode.Space))
         // {
-        //     thrustPercent = thrustPercent > 0 ? 0 : 1f;
+        //     _thrustPercent = _thrustPercent > 0 ? 0 : 1f;
         // }
         //
         // if (Input.GetKeyDown(KeyCode.F))
         // {
-        //     Flap = Flap > 0 ? 0 : 0.3f;
+        //     flap = flap > 0 ? 0 : 0.3f;
         // }
+        //
         // if (Input.GetKeyDown(KeyCode.B))
         // {
-        //     brakesTorque = brakesTorque > 0 ? 0 : 100f;
+        //     _brakesTorque = _brakesTorque > 0 ? 0 : 100f;
         // }
-        // displayText.text = "V: " + ((int)rb.velocity.magnitude).ToString("D3") + " m/s\n";
+        //
+        // displayText.text = "V: " + ((int)_rb.velocity.magnitude).ToString("D3") + " m/s\n";
         // displayText.text += "A: " + ((int)transform.position.y).ToString("D4") + " m\n";
-        // displayText.text += "T: " + (int)(thrustPercent * 100) + "%\n";
-        // displayText.text += brakesTorque > 0 ? "B: ON" : "B: OFF";
+        // displayText.text += "T: " + (int)(_thrustPercent * 100) + "%\n";
+        // displayText.text += _brakesTorque > 0 ? "B: ON" : "B: OFF";
+    }
+
+    public void UpdateInput(float Pitch,float Roll,float Yaw)
+    {
+          pitch = Pitch;
+          roll = Roll;
+          yaw = Yaw;
+
+     
+          
+          
+          
     }
 
     private void FixedUpdate()
--- Assets/_main/scripts/AirplaneInputController.cs
+++ Assets/_main/scripts/AirplaneInputController.cs
@@ -16,26 +16,26 @@ public class AirplaneInputController : MonoBehaviour
     [Header("Pitch")] 
     [SerializeField] private float pitchSensitivity;
     [SerializeField] private float pitchDeadZone;
-    // private void Update()
-    // {
-    //     //высчитываем поворот влево право roll
-    //     var roll = (leftHand.localPosition.y - rightHand.localPosition.y) * rollSensitivity;
-    //     if (Math.Abs(roll - 0) < rollDeadZone)
-    //     {
-    //         roll = 0;
-    //     }
-    //
-    //     var pitch = center.localPosition.y - ((leftHand.localPosition.y + rightHand.localPosition.y) / 2) * pitchSensitivity;
-    //
-    //     if (Math.Abs(pitch - 0) < pitchDeadZone)
-    //     {
-    //         pitch = 0;
-    //     }
-    //     
-    //     
-    //     
-    //     
-    //     
-    //     airplaneController.UpdateInput(pitch,roll,0);
-    // }
+       private void Update()
+       {
+             //высчитываем поворот влево право roll
+           var roll = (leftHand.localPosition.y - rightHand.localPosition.y) * rollSensitivity;
+           if (Math.Abs(roll - 0) < rollDeadZone)
+           {
+               roll = 0;
+           }
+      
+           var pitch = center.localPosition.y - ((leftHand.localPosition.y + rightHand.localPosition.y) / 2) * pitchSensitivity;
+      
+           if (Math.Abs(pitch - 0) < pitchDeadZone)
+           {
+               pitch = 0;
+           }
+           
+           
+           
+           
+           
+           airplaneController.UpdateInput(pitch,roll,0);
+       }
 }
